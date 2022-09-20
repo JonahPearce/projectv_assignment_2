3e01beaa969be1ac5d75f6659d19c7af588a5aae
Jonah Pearce <Jpearce1558@conestogac.on.ca>
2022-09-20 13:49:43 -0400

    Created the Initial Source Code for reading from a file and writing the data to vector.

--- ProjectV_Assignment_2/Source.cpp
+++ ProjectV_Assignment_2/Source.cpp
@@ -1,8 +1,39 @@
 // Project V - Assignment #2
 // Jonah Pearce - September 20th, 2022
 
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+// Student data structure
+struct STUDENT_DATA {
+	string firstname;
+    string lastname;
+};
 
 int main() {
+	
+	//Vector for storing all student data
+	vector<STUDENT_DATA> data;
+
+	ifstream getFile("StudentData.txt");
+
+	if (getFile.is_open()) {
+		string line;
+		while (getline(getFile, line)) {
+			stringstream getLine(line);
+
+			STUDENT_DATA student;
+			// Parsing student data from the files using the commas.
+			getline(getLine, student.lastname, ',');
+			getline(getLine, student.firstname, ',');
+
+			data.push_back(student);
+		}
+	}
 
 	return 1;
 }
